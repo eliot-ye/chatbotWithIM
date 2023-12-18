d4dc982edf4f2b0b672a66103ef35588b6cb0678
Eliot <eliot_ye@163.com>
2023-12-18 16:58:28 +0800

    add

--- src/routes/WeChatCom/index.ts
+++ src/routes/WeChatCom/index.ts
@@ -1,6 +1,7 @@
 import { decrypt, getSignature } from "@wecom/crypto";
 import "dotenv/config";
 import { Router } from "express";
+import { XMLParser } from "fast-xml-parser";
 
 const router = Router();
 
@@ -24,6 +25,32 @@ router.get("/v1", (req, res) => {
     console.log("message", message);
     // 返回 message 信息
     res.send(message);
+  }
+});
+
+router.post("/v1", (req, res) => {
+  const parseXML = new XMLParser();
+  const { msg_signature, timestamp, nonce } = req.query;
+  const echostr: string = parseXML.parse(req.body).Encrypt;
+
+  const signature = getSignature(
+    process.env.wechatcom_token,
+    timestamp as string,
+    nonce as string,
+    echostr as string
+  );
+
+  if (signature === msg_signature) {
+    console.info("签名验证成功");
+
+    // 将加密消息体进行解密，解密后仍旧是 xml 字符串
+    const messageXML = decrypt(process.env.wechatcom_encodingAESKey, echostr);
+    // 把解密后 xml 消息体字符串，解析成 json
+    let callbackDataBody = parseXML.parse(messageXML.message);
+
+    console.log("callbackDataBody", callbackDataBody);
+
+    res.send("666");
   }
 });
 
